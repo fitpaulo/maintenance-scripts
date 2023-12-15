0f5010cf419de7cb5be75cdf91442b9a6ba4d097
Paulo <paulotechusa@protonmail.com>
2023-12-15 12:33:23 -0600

    Fixed bugs and added back in the hidden files code

--- src/sync-dotfiles.sh
+++ src/sync-dotfiles.sh
@@ -5,9 +5,20 @@ file_extensions="$script_config_dir/config-file-extensions"
 subdirs="$script_config_dir/config-subdirs"
 bash="$script_config_dir/bash"
 
-input="$HOME"
+input="$HOME/.config/"
 output="$HOME/src/dotfiles"
 
-rsync -aP --include-from="$file_extensions" --include-from="$subdirs" --exclude='*' "$input" "$output"
-rsync -aP --include-from="$bash" --exclude='*' "$HOME" "$output"
-rsync -aP --include="*.sh" --exclude='*' "$HOME/.bashrc.d/" "$output"
+hidden_files=(
+  ".bashrc"
+  ".bash_profile"
+  ".profile"
+)
+
+for item in "${hidden_files[@]}"
+do
+  # we do the following to remove the . from the filenames
+  rsync -aP "$HOME/$item" "$output/${item:1}" --delete-after
+done
+
+rsync -aP --include-from="$file_extensions" --include-from="$subdirs" --exclude='*' --no-links --delete-after "$input" "$output"
+rsync -aP --include="*.sh" --exclude='*' "$HOME/.bashrc.d/" "$output" --delete-after
