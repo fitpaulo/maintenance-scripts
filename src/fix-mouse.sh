42e90779c8836c53c5ba726befe20cf490db0ba7
Paulo Guimaraes <paulotech_usa@proton.me>
2024-03-20 07:21:48 -0500

    Fixed comment in fix-mouse script

--- src/fix-mouse.sh
+++ src/fix-mouse.sh
@@ -1,6 +1,6 @@
 !#/bin/bash
 
-# Sometimes the left click stops working. I should probably figrue that out
+# Sometimes the right click stops working. I should probably figrue that out
 # This will fix it
 
 xmodmap -e "pointer = 1 2 3"
